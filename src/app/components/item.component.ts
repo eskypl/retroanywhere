a53fdc1464daf62543c8f091fa7d787bb5985b62
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2016-06-03 11:30:11 +0200

    show who edits an item

--- src/app/components/item.component.ts
+++ src/app/components/item.component.ts
@@ -20,14 +20,24 @@ import {FirebaseService} from '../services/firebase.service';
       font-family: 'Ubuntu', sans-serif;
       font-weight: bold;
     }
+    .edited-by-section {
+      height: 60px;
+    }
+    .edited-by-image {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+    }
   `],
   template: `
-    <textarea [ngModel]="text" (ngModelChange)="updateText($event)"></textarea>
+    <textarea [ngModel]="text" (ngModelChange)="updateText($event)" (focus)="onFocus()" (blur)="onBlur()"></textarea>
+    <div class="edited-by-section">Edited by: <img class="edited-by-image" *ngIf="isEditedBy" [src]="isEditedBy.photoURL"/> {{isEditedBy?.name || 'Nobody'}}</div>
   `
 })
 export class ItemComponent {
   @Input() uid;
   text: string;
+  isEditedBy = null;
 
   constructor(private fb: FirebaseService, private ref: ChangeDetectorRef) {}
 
@@ -36,9 +46,27 @@ export class ItemComponent {
       this.text = snapshot.val();
       this.ref.detectChanges();
     });
+
+    this.fb.ref(`items/${this.uid}/isEditedBy`).on('value', (snapshot) => {
+      this.isEditedBy = snapshot.val();
+      this.ref.detectChanges();
+    });
   }
 
   updateText(text: string) {
     this.fb.ref(`items/${this.uid}/text`).set(text);
+  }
+
+  onFocus() {
+    let currentUser = this.fb.currentUser;
+
+    this.fb.ref(`items/${this.uid}/isEditedBy`).set({
+      name: currentUser.displayName,
+      photoURL: currentUser.photoURL
+    });
+  }
+
+  onBlur() {
+    this.fb.ref(`items/${this.uid}/isEditedBy`).set(null);
   }
 }
